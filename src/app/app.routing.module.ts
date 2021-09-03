5276fcbdb039af5ce81ed49854c9eb04f25f937f
Mu Idrees <idris@y7mail.com>
2021-09-03 18:03:44 +0930

    basic routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PasswordHomeComponent } from './passwordapp/password-home/password-home.component';
+import { PostsHomeComponent } from './postsapp/posts-home/posts-home.component';
+import { TypingHomeComponent } from './typingapp/typing-home/typing-home.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'password', component: PasswordHomeComponent },
+  { path: 'typing', component: TypingHomeComponent },
+  { path: 'posts', component: PostsHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
