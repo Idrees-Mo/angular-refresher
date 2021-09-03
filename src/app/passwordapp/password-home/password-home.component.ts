3a1ffdf3aec88de8b1c06ecbfc3b39c58294b807
Mu Idrees <idris@y7mail.com>
2021-09-03 23:27:15 +0930

    add password genrator app

--- src/app/passwordapp/password-home/password-home.component.ts
+++ src/app/passwordapp/password-home/password-home.component.ts
@@ -3,13 +3,58 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-password-home',
   templateUrl: './password-home.component.html',
-  styleUrls: ['./password-home.component.css']
+  styleUrls: ['./password-home.component.css'],
 })
-export class PasswordHomeComponent implements OnInit {
+export class PasswordHomeComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
